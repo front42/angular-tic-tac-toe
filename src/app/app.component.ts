16b650a6008a1dd566939757167255070c69e473
front42 <altaifront@gmail.com>
2025-07-13 19:10:23 +0300

    refactor: upgrade to angular 19 with new features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren, WritableSignal, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [],
 })
 export class AppComponent {
-  counter = 0;
-  slogan = 'Tic-Tac-Toe';
-  table!: HTMLTableElement;
-  cells!: HTMLTableCellElement[];
-  button!: HTMLButtonElement;
+  protected counter: WritableSignal<number> = signal(0);
+  protected gameOver: WritableSignal<boolean> = signal(false);
+  protected slogan: WritableSignal<string> = signal('Tic-Tac-Toe');
+  @ViewChildren('cell') protected cells!: QueryList<ElementRef<HTMLTableCellElement>>;
 
-  marker(event: MouseEvent) {
-    this.table = document.querySelector('table') as HTMLTableElement;
-    this.cells = document.querySelectorAll('td') as unknown as HTMLTableCellElement[];
-    this.button = document.querySelector('button') as HTMLButtonElement;
-    this.button.style.pointerEvents = 'auto';
+  constructor() {}
 
-    const targetCell = event.target as HTMLTableCellElement;
-    targetCell.textContent = ['X', 'O'][this.counter++ % 2];
-    targetCell.style.pointerEvents = 'none';
+  protected mark(cell: HTMLTableCellElement) {
+    cell.textContent = ['X', 'O'][this.counter() % 2];
+    cell.classList.add('disabled');
 
-    this.slogan = `Next ${['X', 'O'][this.counter % 2]}`;
+    this.counter.update((value) => value + 1);
+    this.slogan.set(`Next ${['X', 'O'][this.counter() % 2]}`);
 
-    if (this.isWinner(this.cells)) {
-      this.cells.forEach(cell => cell.style.pointerEvents = 'none');
-      this.slogan = `Winner ${targetCell.textContent}`;
-      this.table.style.boxShadow = '0 0 15px 1px #ffeeb5';
-    } else if (this.counter === 9) {
-      this.cells.forEach(cell => cell.style.backgroundColor = '#04456b');
-      this.slogan = 'Win-Win!';
-      this.table.style.boxShadow = '0 0 15px 1px #ffeeb5';
+    const cells = this.cells.map((elRef: ElementRef) => elRef.nativeElement);
+
+    if (this.isWinner(cells)) {
+      this.gameOver.set(true);
+      this.slogan.set(`Winner ${cell.textContent}`);
+    } else if (this.counter() === 9) {
+      cells.forEach((cell) => cell.classList.add('winning'));
+      this.gameOver.set(true);
+      this.slogan.set('Win-Win!');
     }
   }
 
-  isWinner(cells: HTMLTableCellElement[]) {
+  protected isWinner(cells: HTMLTableCellElement[]) {
     const winningCombs = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8],
-      [0, 3, 6], [1, 4, 7], [2, 5, 8],
-      [0, 4, 8], [2, 4, 6]
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
     ];
 
     for (let comb of winningCombs) {
@@ -47,22 +51,20 @@ export class AppComponent {
         cells[comb[0]].textContent === cells[comb[2]].textContent &&
         cells[comb[0]].textContent !== ''
       ) {
-        [cells[comb[0]], cells[comb[1]], cells[comb[2]]].forEach(cell => cell.style.backgroundColor = '#04456b');
+        [cells[comb[0]], cells[comb[1]], cells[comb[2]]].forEach((cell) => cell.classList.add('winning'));
         return true;
       }
     }
     return false;
   }
 
-  startAgain() {
-    this.counter = 0;
-    this.slogan = 'Tic-Tac-Toe';
-    this.table.style.boxShadow = 'none';
-    this.button.style.pointerEvents = 'none';
-    this.cells.forEach(cell => {
-      cell.textContent = '';
-      cell.style.backgroundColor = '';
-      cell.style.pointerEvents = 'auto';
+  protected reset() {
+    this.counter.set(0);
+    this.gameOver.set(false);
+    this.slogan.set('Tic-Tac-Toe');
+    this.cells.forEach((cell) => {
+      cell.nativeElement.textContent = '';
+      cell.nativeElement.classList.remove('disabled', 'winning');
     });
   }
 }
